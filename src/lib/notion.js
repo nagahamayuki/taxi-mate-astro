5d8854cb4bd1588765945aa854ab42f9486e8683
nagahamayuki <yuuki4104453@gmail.com>
2025-06-09 14:08:00 +0900

    .astroファイルでもPrettierで整形できるようにした

--- src/lib/notion.js
+++ src/lib/notion.js
@@ -1,5 +1,5 @@
-import 'dotenv/config'
-import { Client } from '@notionhq/client';
+import "dotenv/config";
+import { Client } from "@notionhq/client";
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
@@ -11,13 +11,19 @@ export async function getJobsFromNotion() {
   return response.results.map((page) => {
     return {
       id: page.id,
-      title: page.properties['No.']?.title?.[0]?.plain_text || '求人タイトルなし',
-      company: page.properties['会社名']?.rich_text?.[0]?.plain_text || '',
-      location: page.properties['営業所（勤務地）']?.rich_text?.[0]?.plain_text || '',
-      group: page.properties['グループ']?.multi_select?.map(opt => opt.name) || [],
-      status: page.properties['募集状況']?.select?.name || '',
-      dispatchApps: page.properties['配車アプリ・無線']?.multi_select?.map(opt => opt.name) || [],
-      headOffice: page.properties['本社']?.rich_text?.[0]?.plain_text || '',
+      title:
+        page.properties["No."]?.title?.[0]?.plain_text || "求人タイトルなし",
+      company: page.properties["会社名"]?.rich_text?.[0]?.plain_text || "",
+      location:
+        page.properties["営業所（勤務地）"]?.rich_text?.[0]?.plain_text || "",
+      group:
+        page.properties["グループ"]?.multi_select?.map((opt) => opt.name) || [],
+      status: page.properties["募集状況"]?.select?.name || "",
+      dispatchApps:
+        page.properties["配車アプリ・無線"]?.multi_select?.map(
+          (opt) => opt.name
+        ) || [],
+      headOffice: page.properties["本社"]?.rich_text?.[0]?.plain_text || "",
       notionUrl: page.public_url || page.url,
     };
   });

f2a7a442cd4b86253a05c616a8390ca66e58adc3
nagahamayuki <yuuki4104453@gmail.com>
2025-06-11 13:01:48 +0900

    Vercel用のアダプターをインストール@astrojs/vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({
+    imageService: true,
+  }),
   trailingSlash: 'always',
 
   vite: {
